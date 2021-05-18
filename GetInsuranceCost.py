7b47458b94cacc54263bb15d6a2d885664b2ae7b
btierra <btierra@ucsd.edu>
2021-05-18 16:40:04 -0700

    check if data already exists in csv output file

--- GetInsuranceCost.py
+++ GetInsuranceCost.py
@@ -1,3 +1,4 @@
+import os
 import re
 import pandas as pd
 import csv
@@ -45,6 +46,11 @@ class WebScraper:
         self.out_net = self.driver.find_element_by_css_selector('[class="circle out-net-summary"]').text
         self.in_net = self.driver.find_element_by_css_selector('[class="circle in-net-summary"]').text
 
+# Check if data already stored in csv file
+def check_row(reader, zip, code):
+    row_exists = any((row[0] == code) and (row[1] == zip) for row in reader)
+    return row_exists
+
 # 1. Get zip codes
 csv_file = r'C:\Users\Beatrice Tierra\Documents\Springboard\US-Hospital-Charges\Datasets\Addresses.csv'
 address_df = pd.read_csv(csv_file)
@@ -54,33 +60,43 @@ zipcodes = [re.search('CA \d\d\d\d\d', add).group(0).split(' ')[1] for add in ad
 settings = initialize_VPN(save=1,area_input=['complete rotation'])
 rotate_VPN(settings)
 
-# 3. Get cost after insurance 
+# 3. Create csv file to store data
+filename = 'CoverageCost_test2.csv'
+if not os.path.exists(filename):
+    open(filename, 'w').close()
+
+# 4. Get cost after insurance 
+csv_headers = ['CPT Code', 'Zip', 'In-Network', 'Out-Network']
 codes = ['99282', '99283', '99284', '99285']
-with open('CoverageCost_test2.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['CPT Code', 'Zip', 'In-Network', 'Out-Network'])
+
+with open(filename, "r") as rf, open(filename, "a", newline='') as wf:
+    reader = csv.reader(rf)
+    writer = csv.writer(wf)
+    if not check_row(reader, 'Zip', 'CPT Code'):
+        writer.writerow(csv_headers)
     for code in codes:
-        for i, zip in enumerate(zipcodes): 
-            try: 
-                # open browser and search zip and cpt code
-                browser = WebScraper(zip, code)      
-                browser.enter_location() 
-                browser.submit_location()   
-                browser.enter_cpt()    
-                browser.agree_to_terms()   
-                browser.go_to_estimator()                               
-                browser.get_estimates()       
-                in_net, out_net = browser.in_net, browser.out_net             
-            except selenium.common.exceptions.ElementNotInteractableException: 
-                in_net, out_net = 'N/A', 'N/A'
+        for i, zip in enumerate(zipcodes):
+            if not check_row(reader, zip, code):
+                try: 
+                    # open browser and search zip and cpt code
+                    browser = WebScraper(zip, code)      
+                    browser.enter_location() 
+                    browser.submit_location()   
+                    browser.enter_cpt()    
+                    browser.agree_to_terms()   
+                    browser.go_to_estimator()                               
+                    browser.get_estimates()       
+                    in_net, out_net = browser.in_net, browser.out_net             
+                except selenium.common.exceptions.ElementNotInteractableException: 
+                    in_net, out_net = 'N/A', 'N/A'
 
-            # Write to csv
-            writer.writerow([code, zip, in_net, out_net])
+                # Write to csv
+                writer.writerow([code, zip, in_net, out_net])
 
-            # Close driver and check if VPN needs to be switched
-            browser.driver.close()   
-            if i%5 == 0 and i != 0:
-                rotate_VPN(settings)
-                time.sleep(10)
+                # Close driver and check if VPN needs to be switched
+                browser.driver.close()   
+                if i%5 == 0 and i != 0:
+                    rotate_VPN(settings)
+                    time.sleep(10)
 
 terminate_VPN(instructions=None)
