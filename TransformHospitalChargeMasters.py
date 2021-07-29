5afafa55549e71495f3425742f4a2b6489fa3234
btierra <btierra@ucsd.edu>
2021-07-28 17:55:40 -0700

    divide script to functions to be called in GetChargeMasterList

--- TransformHospitalChargeMasters.py
+++ TransformHospitalChargeMasters.py
@@ -2,17 +2,46 @@ import pandas as pd
 import numpy as np
 import re
 
-def extract_cpt_range():
+def get_charge_master_locations():
+    file_locations = pd.read_csv(r"C:\Users\Beatrice Tierra\Documents\Springboard\US-Hospital-Charges\Datasets\ChargeMasterLocations.csv")
+    filepaths = file_locations['filepath']
+    sheetnames = file_locations['sheetname']
+    return filepaths, sheetnames
+
+def read_charge_masters(filepaths, sheetnames):
+    df = pd.DataFrame()
+    for filepath, sheetname in zip(filepaths, sheetnames):
+        hospital = filepath.split('\\')[-2]
+        df_tmp = pd.read_excel(filepath, sheet_name = sheetname, usecols=[0,1,2], names = ['Description', 'CPT', 'Charge'])
+        df_tmp['CPT'] = df_tmp['CPT'].astype(str)
+        df_tmp = df_tmp.loc[df_tmp.CPT.str.contains('\d\d\d\d\d', na=False),:]
+        df_tmp['Hospital'] = hospital
+        df = df.append(df_tmp)
+    return df
+
+def clean_up(df):
+    ## Ex. ranges => CPT Code 97161-9763 -> 97161, 97162, 97163
+    ## Ex. splits => CPT Code 81002 or 81003 
+    df_range = extract_cpt_range(df)
+    df_split = extract_cpt_split(df)
+    df_int = extract_cpt_int(df)
+
+    # re-combine dataframes
+    df_all = pd.concat([df_range, df_split, df_int], ignore_index=True)
+    df_all.sort_values(by='Hospital', inplace=True)
+    df_all.drop_duplicates(keep='first', inplace=True, ignore_index=True)
+
+def extract_cpt_range(df):
     df_range_tmp = df[df['CPT'].str.contains('-')]
     df_range = create_separate_rows(df_range_tmp, '-')
     return df_range
 
-def extract_cpt_split():
+def extract_cpt_split(df):
     df_split_tmp = df[df['CPT'].str.contains('or|/')]
     df_split = create_separate_rows(df_split_tmp, 'or|/')
     return df_split 
 
-def extract_cpt_int():
+def extract_cpt_int(df):
     df_int = df[~df['CPT'].str.contains('-|or|/')]
     df_int = df_int[~df_int['Description'].str.contains('Facility')]
     df_int = df_int[['Hospital', 'Description', 'CPT', 'Charge']]
@@ -40,31 +69,5 @@ def create_separate_rows(df_tmp, parser):
     df_separated_rows =  pd.DataFrame({'Hospital':hospital, 'Description':desc, 'CPT': cpt, 'Charge': charge})
     return df_separated_rows
 
-# 1. Load data
-file_locations = pd.read_csv(r"C:\Users\Beatrice Tierra\Documents\Springboard\US-Hospital-Charges\Datasets\ChargeMasterLocations.csv")
-filepaths = file_locations['filepath']
-sheetnames = file_locations['sheetname']
-
-# 2. Read excel sheets given in df
-global df
-df = pd.DataFrame()
-for filepath, sheetname in zip(filepaths, sheetnames):
-    hospital = filepath.split('\\')[-2]
-    df_tmp = pd.read_excel(filepath, sheet_name = sheetname, usecols=[0,1,2], names = ['Description', 'CPT', 'Charge'])
-    df_tmp['CPT'] = df_tmp['CPT'].astype(str)
-    df_tmp = df_tmp.loc[df_tmp.CPT.str.contains('\d\d\d\d\d', na=False),:]
-    df_tmp['Hospital'] = hospital
-    df = df.append(df_tmp)
-
-# 3. Clean up CPT column
-## Ex. ranges => CPT Code 97161-9763 -> 97161, 97162, 97163
-## Ex. splits => CPT Code 81002 or 81003 
-df_range = extract_cpt_range()
-df_split = extract_cpt_split()
-df_int = extract_cpt_int()
-
 # 4. Merge separate dataframes into one
-df_all = pd.concat([df_range, df_split, df_int], ignore_index=True)
-df_all.sort_values(by='Hospital', inplace=True)
-df_all.drop_duplicates(keep='first', inplace=True, ignore_index=True)
 df_all.to_csv(r"C:\Users\Beatrice Tierra\Documents\Springboard\US-Hospital-Charges\Datasets\ChargeMasterList.csv", index=False)
