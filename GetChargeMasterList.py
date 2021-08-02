fd6cc8180c81bf2c0f1c983f0c0fe356db46f743
Beatrice Tierra <beatricetierra@gmail.com>
2021-08-02 16:18:45 -0700

    Update to read HosptitalProfiles.csv

--- GetChargeMasterList.py
+++ GetChargeMasterList.py
@@ -1,11 +1,12 @@
+import os
 import sys
 from ExtractHospitalChargeMasters import find_form_locations
 from TransformHospitalChargeMasters import export_charge_masters
 
 if __name__ == "__main__":
     charge_master_folderpath = str(sys.argv[1])
-    extract_results = str(sys.argv[2])
-    transform_results = str(sys.argv[3])
+    results = str(sys.argv[2])
+    intermediate_file = os.path.join(charge_master_folderpath, "Locations.csv")
 
-    find_form_locations(charge_master_folderpath, extract_results)
-    export_charge_masters(extract_results, transform_results)
+    find_form_locations(charge_master_folderpath, intermediate_file)
+    export_charge_masters(intermediate_file, results)
